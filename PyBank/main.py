ce453ad2396323c8bd09896d34115fcc2da8e677
Rohan Patel <rpatel10491@gmail.com>
2020-09-18 14:27:52 -0400

    new

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,31 +5,27 @@ import os
 #read budget.data.csv file
 budget_data = os.path.join("Resources", "budget_data.csv")
 
-#number of months
-total_months = 0
-#total profit loss
-total_profitLoss = 0
-#average change
-change = 0
-#initialize titles/rows list
-date = []
-profit = []
-
-
-
-
 #read CSV file and create csv reader
 with open(budget_data) as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ",")
+  #  print(csvreader)
     csv_header = next(csvreader)
-    print(csvreader)
+    print(csv_header)
 
+#number of months
+#first, create variables for total months and total revenue
+    total_months = []
+    total_revenue = []
+#use loop to read csv
+    for row in csvreader:
+#use append to add element to end of list, starts row 2; indexes are 0 and 1
+        total_months.append(row[0])
+        total_revenue.append(row[1])
+#print length of total months
+    print(len(total_months))
 
-
-
-
-
-
+#calculate net total amount of profit/losses
+    total_revenue + (sum())
 
 #print("Financial Analysis")
 #print("----------------------------")
