4ecc02a87b9290040282a81bce7c0850abbd806e
Rohan Patel <rpatel10491@gmail.com>
2020-09-18 20:18:26 -0400

    latest

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,6 +5,7 @@ import os
 #read budget.data.csv file
 budget_data = os.path.join("Resources", "budget_data.csv")
 
+
 #read CSV file and create csv reader
 with open(budget_data) as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ",")
@@ -16,21 +17,30 @@ with open(budget_data) as csvfile:
 #first, create variables for total months and total revenue
     total_months = []
     total_revenue = []
+    change = []
 #use loop to read csv
     for row in csvreader:
 #use append to add element to end of list, starts row 2; indexes are 0 and 1
         total_months.append(row[0])
-        total_revenue.append(row[1])
+        total_revenue.append(int(row[1]))
 #print length of total months
-    print(len(total_months))
+    length = len(total_months)
+    print(length)
+#total profit/loss
+    sum = (sum(total_revenue))
+    revenue_changes = []
+    for i in range(len(total_revenue) - 1)
+#average of changes
+    average = sum/length
+    print(average)
 
-#calculate net total amount of profit/losses
-    total_revenue + (sum())
+#maximum increase in profits
+    maximum = max(total_revenue)
 
-#print("Financial Analysis")
-#print("----------------------------")
-#print("Total Months: ")
-#print("Total: ")
-#print("Average Change: ")
-#print("Greatest Increase in Profits: ")
-#print("Greatest Decrease in Profits: ")
+print("Financial Analysis")
+print("----------------------------")
+print("Total Months: ", length)
+print("Total: $", sum)
+print("Average Change: ", average)
+print("Greatest Increase in Profits: ")
+print("Greatest Decrease in Profits: ")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,3 +1,18 @@
 import csv
 import os
 
+#read election.data.csv file
+election_data = os.path.join("Resources", "budget_data.csv")
+
+#read CSV file and create csv reader
+with open(election_data) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = ",")
+  #  print(csvreader)
+    csv_header = next(csvreader)
+    print(csv_header)
+
+#1 - count length of the list
+#2 - https://stackoverflow.com/questions/12282232/how-do-i-count-unique-values-inside-a-list
+#3 - #2 votes/#1 total 
+#4 - #3 * #2
+#5 - max total
